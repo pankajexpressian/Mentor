d197f55bf1a01e95db3744b59daea1aa676377f2
Pankaj Jangid <pankaj.jangid>
2020-02-14 15:25:27 +0530

    Dummy Changes

--- Data/Migrations/20200214095235_addDummyTable.Designer.cs
+++ Data/Migrations/20200214095235_addDummyTable.Designer.cs
@@ -4,14 +4,16 @@ using Mentor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Mentor.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200214095235_addDummyTable")]
+    partial class addDummyTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
